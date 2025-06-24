f9eaead5d8e52b854dc155cdcde19edbc2efa13e
Ryuji Ogawa <dragonogawaz@gmail.com>
2025-06-25 00:54:30 +0900

    AppコンポーネントにAPIから気象情報を取得するコードを移す

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import Title from './components/Title';
-import Form from './components/Form';
+import { useState } from "react";
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Results from "./components/Results";
 
 const App = () => {
-    return (
+  const [city, setCity] = useState("");
+  const [results, setResult] = useState({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
+
+  const getWeather = (e) => {
+    e.preventDefault();
+    fetch("https://api.weatherapi.com/v1/current.json?key=4eead6ff008a492b8d4130745252206&q=London&aqi=no")
+      .then((res) => res.json())
+      .then((data) =>
+        setResult({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        })
+      );
+  };
+
+  return (
     <div>
-        <Title />
-        <Form />
+      <Title />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results results={results} />
     </div>
-    );
-}
+  );
+};
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,12 @@
-import {useState} from "react";
-
-const Form = () => {
-    const [city, setCity] = useState("");
-    const getWeather = (e) => {
-        e.preventDefault();
-        fetch("https://api.weatherapi.com/v1/current.json?key=4eead6ff008a492b8d4130745252206&q=London&aqi=no")
-        .then(res => res.json())
-        .then(data => console.log(data));
-    }
-
-    return(
-        <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-            <button type="submit" onClick={getWeather}>Get Weather</button>
-        </form>
-    );
-}
+const Form = (props) => {
+  return (
+    <form>
+      <input type="text" name="city" placeholder="都市名" onChange={(e) => props.setCity(e.target.value)} />
+      <button type="submit" onClick={props.getWeather}>
+        Get Weather
+      </button>
+    </form>
+  );
+};
 
 export default Form;
